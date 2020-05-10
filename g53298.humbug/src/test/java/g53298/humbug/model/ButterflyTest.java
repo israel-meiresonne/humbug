fbc33ded1fad16f7a883a8ccdcf98464ee3bcf93
Israel Meiresonne <israelmeiresonne97@gmail.com>
2020-05-10 21:19:49 +0200

    correction of the ButterflyTest class and replace Papillon by Butterfly for check up

--- g53298.humbug/src/test/java/g53298/humbug/model/ButterflyTest.java
+++ g53298.humbug/src/test/java/g53298/humbug/model/ButterflyTest.java
@@ -35,169 +35,169 @@ public class ButterflyTest {
     @Test
     public void testMove() {
         System.out.println("move_general");
-//        Papillon instance = (Papillon) animals[0];
-//        Position expResult = new Position(0, 3);
-//        Position result = instance.move(board, Direction.EAST, animals);
-//        assertEquals(expResult, result);
-//    }
-//
-//    /**
-//     * Test of move method, of class Butterfly.
-//     */
-//    @Test
-//    public void testMove_endline() {
-//        System.out.println("move end line and fall");
-//        board = new Board(new Square[][]{
-//            {new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {null, new Square(GRASS), new Square(GRASS)},
-//            {null, null, new Square(STAR)}
-//        });
-//        Papillon instance = (Papillon) animals[0];
-//        instance.setPositionOnBoard(new Position(0, 0));
-//        Position expResult = null;
-//        Position result = instance.move(board, Direction.EAST, animals);
-//        assertEquals(expResult, result);
-//    }
-//
-//    /**
-//     * Test of move method, of class Butterfly.
-//     */
-//    @Test
-//    public void testMove_next_notfree() {
-//        System.out.println("move next case not free");
-//        Papillon instance = (Papillon) animals[0];
-//        animals[1].setPositionOnBoard(new Position(0, 1));
-//        Position expResult = new Position(0, 3);
-//        Position result = instance.move(board, Direction.EAST, animals);
-//        assertEquals(expResult, result);
-//    }
-//
-//    /**
-//     * Test of move method, of class Butterfly.
-//     */
-//    @Test
-//    public void testMove_landingOnAnimal() {
-//        System.out.println("move and landing on other animal");
-//        board = new Board(new Square[][]{
-//            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS),},
-//            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {null, null, new Square(GRASS), new Square(GRASS), new Square(STAR)}
-//        });
-//        animals[1] = new Snail(new Position(0, 3));
-//        Papillon instance = (Papillon) animals[0];
-//        Position expResult = new Position(0, 4);
-//        Position result = instance.move(board, Direction.EAST, animals);
-//        assertEquals(expResult, result);
-//    }
-//
-//    /**
-//     * Test of move method, of class Butterfly.
-//     */
-//    @Test
-//    public void testMove_flyUpTwoEmptySquare() {
-//        System.out.println("move next two null case by flying");
-//        board = new Board(new Square[][]{
-//            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {new Square(GRASS), null, new Square(GRASS), new Square(STAR)}
-//        });
-//        Papillon instance = (Papillon) animals[0];
-//        Position expResult = new Position(3, 0); // landing
-//        Position result = instance.move(board, Direction.SOUTH, animals);
-//        assertEquals(expResult, result);
-//    }
-//
-//    /**
-//     * Test of move method, of class Butterfly.
-//     */
-//    @Test
-//    public void testMove_flyUpThreeEmptySquare() {
-//        System.out.println("move next three null case by flying");
-//        board = new Board(new Square[][]{
-//            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {new Square(GRASS), null, new Square(GRASS), new Square(STAR)}
-//        });
-//        Papillon instance = (Papillon) animals[0];
-//        instance.setPositionOnBoard(new Position(3, 0));
-//        Position expResult = null; // fall
-//        Position result = instance.move(board, Direction.NORTH, animals);
-//        assertEquals(expResult, result);
-//    }
-//
-//    /**
-//     * Test of move method, of class Butterfly.
-//     */
-//    @Test
-//    public void testMove_passOnStar() {
-//        System.out.println("move and pass on star without win");
-//        board = new Board(new Square[][]{
-//            {new Square(GRASS), new Square(GRASS), new Square(STAR), new Square(GRASS)},
-//            {null, new Square(GRASS), new Square(GRASS), null},
-//            {null, null, new Square(STAR), null}
-//        });
-//        animals = new Animal[]{
-//            new Papillon(new Position(0, 0)),
-//            new Snail(new Position(1, 3))
-//        };
-//        Papillon instance = (Papillon) animals[0];
-//        Position expResult = new Position(0, 3);
-//        Position result = instance.move(board, Direction.EAST, animals);
-//        assertEquals(expResult, result);
-//        assertFalse(animals[0].isOnStar());
-//        assertFalse(board.getSquareType(new Position(0, 2)) == GRASS);
-//    }
-//
-//    /**
-//     * Test of move method, of class Butterfly.
-//     */
-//    @Test
-//    public void testMove_nextOnStar() {
-//        System.out.println("move, next on star and win");
-//        board = new Board(new Square[][]{
-//            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(STAR)},
-//            {null, new Square(GRASS), new Square(GRASS), null},
-//            {null, null, new Square(STAR), null}
-//        });
-//        animals = new Animal[]{
-//            new Papillon(new Position(0, 0)),
-//            new Snail(new Position(1, 3))
-//        };
-//        Papillon instance = (Papillon) animals[0];
-//        Position expResult = new Position(0, 3);
-//        Position result = instance.move(board, Direction.EAST, animals);
-//        assertEquals(expResult, result);
-//        assertTrue(animals[0].isOnStar());
-//        assertEquals(GRASS, board.getSquareType(result));
-//    }
-//
-//    /**
-//     * Test of move method, of class Butterfly.
-//     */
-//    @Test
-//    public void testMove_jumpUpWall() {
-//        System.out.println("fly up wall");
-//        Square sqEastWall = new Square(GRASS);
-//        sqEastWall.setEastWall(true);
-//        Square sqWestWall = new Square(GRASS);
-//        sqWestWall.setWestWall(true);
-//        board = new Board(new Square[][]{
-//            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {new Square(GRASS), sqEastWall, sqWestWall, new Square(GRASS)},
-//            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
-//            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(STAR)}
-//        });
-//        animals = new Animal[]{
-//            new Butterfly(new Position(0, 0)),
-//            new Butterfly(new Position(2, 0))
-//        };
-//        Butterfly instance = (Butterfly) animals[1];
-//        Position expResult = new Position(2, 2);
-//        Position result = instance.move(board, Direction.EAST, animals);
-//        assertEquals(expResult, result);
-//    }
+        Butterfly instance = (Butterfly) animals[0];
+        Position expResult = new Position(0, 3);
+        Position result = instance.move(board, Direction.EAST, animals);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of move method, of class Butterfly.
+     */
+    @Test
+    public void testMove_endline() {
+        System.out.println("move end line and fall");
+        board = new Board(new Square[][]{
+            {new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {null, new Square(GRASS), new Square(GRASS)},
+            {null, null, new Square(STAR)}
+        });
+        Butterfly instance = (Butterfly) animals[0];
+        instance.setPositionOnBoard(new Position(0, 0));
+        Position expResult = null;
+        Position result = instance.move(board, Direction.EAST, animals);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of move method, of class Butterfly.
+     */
+    @Test
+    public void testMove_next_notfree() {
+        System.out.println("move next case not free");
+        Butterfly instance = (Butterfly) animals[0];
+        animals[1].setPositionOnBoard(new Position(0, 1));
+        Position expResult = new Position(0, 3);
+        Position result = instance.move(board, Direction.EAST, animals);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of move method, of class Butterfly.
+     */
+    @Test
+    public void testMove_landingOnAnimal() {
+        System.out.println("move and landing on other animal");
+        board = new Board(new Square[][]{
+            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS),},
+            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {null, null, new Square(GRASS), new Square(GRASS), new Square(STAR)}
+        });
+        animals[1] = new Snail(new Position(0, 3));
+        Butterfly instance = (Butterfly) animals[0];
+        Position expResult = new Position(0, 4);
+        Position result = instance.move(board, Direction.EAST, animals);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of move method, of class Butterfly.
+     */
+    @Test
+    public void testMove_flyUpTwoEmptySquare() {
+        System.out.println("move next two null case by flying");
+        board = new Board(new Square[][]{
+            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {new Square(GRASS), null, new Square(GRASS), new Square(STAR)}
+        });
+        Butterfly instance = (Butterfly) animals[0];
+        Position expResult = new Position(3, 0); // landing
+        Position result = instance.move(board, Direction.SOUTH, animals);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of move method, of class Butterfly.
+     */
+    @Test
+    public void testMove_flyUpThreeEmptySquare() {
+        System.out.println("move next three null case by flying");
+        board = new Board(new Square[][]{
+            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {null, new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {new Square(GRASS), null, new Square(GRASS), new Square(STAR)}
+        });
+        Butterfly instance = (Butterfly) animals[0];
+        instance.setPositionOnBoard(new Position(3, 0));
+        Position expResult = null; // fall
+        Position result = instance.move(board, Direction.NORTH, animals);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of move method, of class Butterfly.
+     */
+    @Test
+    public void testMove_passOnStar() {
+        System.out.println("move and pass on star without win");
+        board = new Board(new Square[][]{
+            {new Square(GRASS), new Square(GRASS), new Square(STAR), new Square(GRASS)},
+            {null, new Square(GRASS), new Square(GRASS), null},
+            {null, null, new Square(STAR), null}
+        });
+        animals = new Animal[]{
+            new Butterfly(new Position(0, 0)),
+            new Snail(new Position(1, 3))
+        };
+        Butterfly instance = (Butterfly) animals[0];
+        Position expResult = new Position(0, 3);
+        Position result = instance.move(board, Direction.EAST, animals);
+        assertEquals(expResult, result);
+        assertFalse(animals[0].isOnStar());
+        assertFalse(board.getSquareType(new Position(0, 2)) == GRASS);
+    }
+
+    /**
+     * Test of move method, of class Butterfly.
+     */
+    @Test
+    public void testMove_nextOnStar() {
+        System.out.println("move, next on star and win");
+        board = new Board(new Square[][]{
+            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(STAR)},
+            {null, new Square(GRASS), new Square(GRASS), null},
+            {null, null, new Square(STAR), null}
+        });
+        animals = new Animal[]{
+            new Butterfly(new Position(0, 0)),
+            new Snail(new Position(1, 3))
+        };
+        Butterfly instance = (Butterfly) animals[0];
+        Position expResult = new Position(0, 3);
+        Position result = instance.move(board, Direction.EAST, animals);
+        assertEquals(expResult, result);
+        assertTrue(animals[0].isOnStar());
+        assertEquals(GRASS, board.getSquareType(result));
+    }
+
+    /**
+     * Test of move method, of class Butterfly.
+     */
+    @Test
+    public void testMove_jumpUpWall() {
+        System.out.println("fly up wall");
+        Square sqEastWall = new Square(GRASS);
+        sqEastWall.setEastWall(true);
+        Square sqWestWall = new Square(GRASS);
+        sqWestWall.setWestWall(true);
+        board = new Board(new Square[][]{
+            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {new Square(GRASS), sqEastWall, sqWestWall, new Square(GRASS)},
+            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(GRASS)},
+            {new Square(GRASS), new Square(GRASS), new Square(GRASS), new Square(STAR)}
+        });
+        animals = new Animal[]{
+            new Butterfly(new Position(0, 0)),
+            new Butterfly(new Position(2, 0))
+        };
+        Butterfly instance = (Butterfly) animals[1];
+        Position expResult = new Position(2, 3);
+        Position result = instance.move(board, Direction.EAST, animals);
+        assertEquals(expResult, result);
+    }
     
 }
